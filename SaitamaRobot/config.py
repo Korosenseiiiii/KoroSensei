1510797110e6d175025bd2c034c67aea89925e66
Korosenseiiiii <138607833+Korosenseiiiii@users.noreply.github.com>
2023-07-04 22:45:06 +0530

    Update config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -39,14 +39,14 @@ class Config(object):
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    DRAGONS = 
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    DEV_USERS = 
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "supports")
+    DEMONS = 
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "tigers")
-    WOLVES = get_user_list("elevated_users.json", "whitelists")
+    TIGERS = 
+    WOLVES = 
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
